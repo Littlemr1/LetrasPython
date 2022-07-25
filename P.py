6855eb5b5504e5edab6ba80c1b5fb015784bcc1b
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:51:02 -0400

    Create P.py

---
+++ P.py
@@ -0,0 +1,19 @@
+#  ****                                                                   
+#  *   *                                                                  
+#  *   *                                                                  
+#  ****                                                                   
+#  *                                                                      
+#  *                                                                      
+#  *  
+
+for i in range(7):
+    for j in range(7):
+        if (i == 0 or i == 3) and (1 <= j <= 4):
+            print('*', end='')
+        elif (i == 1 or i == 2) and (j == 1 or j == 5):
+            print('*', end='')
+        elif (4 <= i <= 6) and j == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
