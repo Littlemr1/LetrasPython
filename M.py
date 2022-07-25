a72dc3b2cb781a2dd5fb02bb2cca71dcab64706c
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:50:12 -0400

    Create M.py

---
+++ M.py
@@ -0,0 +1,19 @@
+#   *       *                                                             
+#   *       *                                                             
+#   * *   * *                                                             
+#   *   *   *                                                             
+#   *       *                                                             
+#   *       *                                                             
+#   *       *
+
+for i in range(7):
+    for j in range(12):
+        if (i == 0 or i == 1 or 4 <= i <= 6) and (j == 1 or j == 11):
+            print('*', end='')
+        elif i == 2 and (j == 1 or j == 3 or j == 9 or j == 11):
+            print('*', end='')
+        elif i == 3 and (j == 1 or j == 6 or j == 11):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
