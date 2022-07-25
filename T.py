4bfd85d5105813ac3133a7306b7194d995ee5622
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:52:16 -0400

    Create T.py

---
+++ T.py
@@ -0,0 +1,17 @@
+#  *****                                                                  
+#    *                                                                    
+#    *                                                                    
+#    *                                                                    
+#    *                                                                    
+#    *                                                                    
+#    *  
+
+for i in range(7):
+    for j in range(7):
+        if i == 0 and 1 <= j <= 5:
+            print('*', end='')
+        elif i > 0 and j == 3:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
