db476ce1f53fd8e8128820d3b0dfd1b828b6b304
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:51:35 -0400

    Create S.py

---
+++ S.py
@@ -0,0 +1,23 @@
+#   ****                                                                  
+#  *                                                                      
+#  *                                                                      
+#   ***                                                                   
+#      *                                                                  
+#      *                                                                  
+#  **** 
+
+for i in range(7):
+    for j in range(7):
+        if i == 0 and (2 <= j <= 5):
+            print('*', end='')
+        elif (i == 1 or i == 2) and j == 1:
+            print('*', end='')
+        elif i == 3 and 2 <= j <= 4:
+            print('*', end='')
+        elif (i == 4 or i == 5) and j == 5:
+            print('*', end='')
+        elif i == 6 and 1 <= j <= 4:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
