2df82a74d4e0b33e8b71f2316ad72dfc056e6084
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:49:17 -0400

    Create G.py

---
+++ G.py
@@ -0,0 +1,21 @@
+#   ***                                                                   
+#  *   *                                                                  
+#  *                                                                      
+#  * ***                                                                  
+#  *   *                                                                  
+#  *   *                                                                  
+#   *** 
+
+for i in range(7):
+    for j in range(7):
+        if (i == 0 or i == 6) and 2 <= j <= 4:
+            print('*', end='')
+        elif (i == 1 or i == 4 or i == 5) and (j== 1 or j == 5):
+            print('*', end='')
+        elif i == 2 and j == 1:
+            print('*', end='')
+        elif i == 3 and (j == 1 or 3 <= j <= 5):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
