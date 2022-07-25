60e2877d9a40cedf00896ef89f07178b720750aa
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:50:34 -0400

    Create O.py

---
+++ O.py
@@ -0,0 +1,17 @@
+#   ***                                                                   
+#  *   *                                                                  
+#  *   *                                                                  
+#  *   *                                                                  
+#  *   *                                                                  
+#  *   *                                                                  
+#   *** 
+
+for i in range(7):
+    for j in range(7):
+        if (i == 0 or i == 6) and 2 <= j <= 4:
+            print('*', end='')
+        elif 1 <= i <= 5 and (j == 1 or j == 5):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
